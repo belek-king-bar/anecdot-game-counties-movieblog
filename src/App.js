1819ffe8bac0be8d069b7b3a5edfeef1a86f7893
belek <belektolubaev@gmail.com>
2019-02-27 23:40:26 +0600

    добавление контейнеров в App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Country from './Containers/Country/Country.js';
+import MovieBlog from './Containers/MovieBlog/MovieBlog.js';
+import Blog from './Containers/Blog/Blog.js';
+import Game from './Containers/Game/Game.js';
+import Layout from './Layout.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <BrowserRouter>
+            <Layout>
+              <Switch>
+                  <Route path="/game" component={Game}/>
+                  <Route path="/blog" component={Blog}/>
+                  <Route path="/movie" component={MovieBlog}/>
+                  <Route path="/" exact component={Country}/>
+              </Switch>
+            </Layout>
+        </BrowserRouter>
+
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://restcountries.eu/rest/v2/';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
