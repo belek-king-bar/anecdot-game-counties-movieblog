4e5e582bc066e0293d5a82b3ffa9d11792eaf02c
belek <belektolubaev@gmail.com>
2019-02-27 23:38:44 +0600

    Создание контейнера для анекдотов

---
+++ src/Containers/Blog/Blog.js
@@ -0,0 +1,62 @@
+import React, {Component, Fragment} from 'react';
+import './Blog.css';
+import Post from '../../Components/Posts/Post.js';
+
+
+class Blog extends Component {
+    state = {
+        posts: []
+    };
+
+    componentDidMount() {
+        const P_URL = 'https://api.chucknorris.io/jokes/random';
+        let promises = [];
+        const N = 5;
+
+        for (let i = 0; i < N; i++) {
+            let request = fetch(P_URL).then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error('Something went wrong with network request');
+            });
+            promises.push(request);
+        }
+
+        Promise.all(promises).then(posts => {
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post
+                }
+            });
+
+            this.setState({posts: updatedPosts});
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+
+
+    render() {
+
+        return (
+            <Fragment>
+                <section className="Posts">
+                    {this.state.posts.map(post => (
+                        <Post
+                            key={post.id}
+                            icon={post.icon_url}
+                            value={post.value}
+                        />
+                    ))}
+
+                </section>
+
+            </Fragment>
+        )
+    }
+
+}
+
+
+export default Blog;
